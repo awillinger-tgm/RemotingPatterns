c8f6684ac8927f431dc59cadb1501ca2e708f4dc
mike <mike@meschmobile>
2009-05-24 13:58:01 +0200

    Lookup pre-alpha version

--- main/src/main/java/evs2009/Lookup.java
+++ main/src/main/java/evs2009/Lookup.java
@@ -3,16 +3,32 @@ package evs2009;
 import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
 
 /**
- *
- * @author  Michael Borko <michael@borko.at>
- *          Florian Motlik <flomotlik@gmail.com>
- *			Michael Greifeneder <mikegr@gmx.net>
- *
+ * 
+ * @author Michael Borko <michael@borko.at> Florian Motlik <flomotlik@gmail.com>
+ *         Michael Greifeneder <mikegr@gmx.net>
+ * 
  */
 public class Lookup {
 
+	private HashMap<String, String> peers = new HashMap<String, String>();
+	
 	public AbsolutObjectReference getReference(String name) {
 		return new AbsolutObjectReference(name);
+	}
+
+	private init() {
+		CsvReader reader = new CsvReader("peers.csv");
+
+		reader.readHeaders();
+
+		while (reader.readRecord()) {
+			String peerID = reader.get("PeerID");
+			String peerAddress = reader.get("PeerAddress");
+
+			System.out.println("peerID: "+peerID);
+			System.out.println("peerAddress: "+peerAddress);
+		}
+		reader.close();
 	}
 
 }
