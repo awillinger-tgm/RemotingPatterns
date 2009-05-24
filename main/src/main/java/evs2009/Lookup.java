43c45c005dcb59f140c420747ab17c6af261acfd
mike <mike@meschmobile>
2009-05-24 14:15:34 +0200

    changes made for accessing csv

--- main/src/main/java/evs2009/Lookup.java
+++ main/src/main/java/evs2009/Lookup.java
@@ -1,5 +1,10 @@
 package evs2009;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import com.csvreader.*;
+
 import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
 
 /**
@@ -11,24 +16,35 @@ import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
 public class Lookup {
 
 	private HashMap<String, String> peers = new HashMap<String, String>();
-	
+
 	public AbsolutObjectReference getReference(String name) {
 		return new AbsolutObjectReference(name);
 	}
 
-	private init() {
-		CsvReader reader = new CsvReader("peers.csv");
+	//private void init() {
+	public static void main( String[] args) {
+		CsvReader reader;
+		try {
+			reader = new CsvReader("peers.csv");
 
-		reader.readHeaders();
+			reader.readHeaders();
 
-		while (reader.readRecord()) {
-			String peerID = reader.get("PeerID");
-			String peerAddress = reader.get("PeerAddress");
+			while (reader.readRecord()) {
+				String peerID = reader.get("PeerID");
+				String peerAddress = reader.get("PeerAddress");
 
-			System.out.println("peerID: "+peerID);
-			System.out.println("peerAddress: "+peerAddress);
+				System.out.println("peerID: " + peerID);
+				System.out.println("peerAddress: " + peerAddress);
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		reader.close();
+
 	}
 
 }
