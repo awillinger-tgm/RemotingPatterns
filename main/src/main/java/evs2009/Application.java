032d24ce1f6ebc76151c3b325858e8c3305f133c
Michael Greifeneder <mikegr@gmx.net>
2009-05-30 11:58:03 +0200

    Update log4j config

--- main/src/main/java/evs2009/Application.java
+++ main/src/main/java/evs2009/Application.java
@@ -1,11 +1,18 @@
 package evs2009;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
 
+
+	private static final Logger log = LoggerFactory
+			.getLogger(Application.class);
 
 	private final String peerId;
 
 	public Application(String nameList, String peerId) {
+
 		//TODO: handle nameList
 		this.peerId = peerId;
 
@@ -18,10 +25,11 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
+		log.info("Starting application");
 		if (args.length < 1) {
 			System.out.println("Usage Application <namelist> <peerId>");
 		}
 		new Application(args[0], args[1]).run();
-
+		log.info("Application finished");
 	}
 }
